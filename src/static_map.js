251d09e54e009c94ffd45abb1e4d0bd6aa9fd30f
Kikojas Deployer <deployer@kikojas.com>
2018-01-14 12:17:10 +0330

    Updated static map API

--- src/static_map.js
+++ src/static_map.js
@@ -2,28 +2,28 @@
 
 var config = require('./config');
 
-module.exports = function (container, options) {
-// {
-//     alttext: string,
-//     maptype: string,
-//     position: {
-//         lat: 35.79,
-//         lng: 51.41,
-//         zoom: 13
-//     },
-//     dimensions: {
-//         width: 800,
-//         height: 600
-//     }
-//     scale: bool,
-//     qs: [
-//         {
-//             marker_url: '',
-//             lat: 35.79,
-//             lng: 51.41,
-//         }
-//     ]
-// }    
+module.exports = function (options) {
+    // {
+    //     alttext: string,
+    //     maptype: string,
+    //     position: {
+    //         lat: 35.79,
+    //         lng: 51.41,
+    //         zoom: 13
+    //     },
+    //     dimensions: {
+    //         width: 800,
+    //         height: 600
+    //     },
+    //     scale: bool,
+    //     qs: [
+    //         {
+    //             marker_url: '',
+    //             lat: 35.79,
+    //             lng: 51.41,
+    //         }
+    //     ]
+    // } 
     if (!options.maptype) {
         options.maptype = 'light';
     }
@@ -33,21 +33,16 @@ module.exports = function (container, options) {
     if (!options.position) {
         options.position = 'auto';
     } else {
-        if(typeof options.position !== 'object') {
+        if (typeof options.position !== 'object') {
             throw 'Unknown value for position.'
         }
     }
     if (!options.dimensions) {
         throw 'Cedarmaps: No dimensions specified.';
     } else {
-        if(typeof options.dimensions !== 'object') {
+        if (typeof options.dimensions !== 'object') {
             throw 'Unknown value for position.'
         }
-    }
-    if (!container) {
-        throw 'Cedarmaps: No container selector specified.';
-    } else if (!document.querySelector(container)) {
-        throw 'Cedarmaps: Could not find element ' + options.container;
     }
     var position = options.position.lat ? (options.position.lat + ',' + options.position.lng + ',' + options.position.zoom) : options.position;
     var baseUrl = (document.location.protocol === 'https:' || config.FORCE_HTTPS) ? config.HTTPS_URL : config.HTTP_URL;
@@ -55,20 +50,37 @@ module.exports = function (container, options) {
         '/' + position + '/' + options.dimensions.width + 'x' + options.dimensions.height +
         (options.scale ? '@2x' : '') + '?access_token=' + L.cedarmaps.accessToken;
     if (options.qs && options.qs.length > 0) {
-        var qs = 'markers=';
+        var param = []
         for (var i = 0, l = options.qs.length; i < l; i++) {
             if (!options.qs[i].marker_url) {
                 options.qs[i].marker_url = 'marker-default';
             }
-            qs += options.qs[i].marker_url + '|' + options.qs[i].lat + ',' + options.qs[i].lng + '|';
+            param.push(options.qs[i].marker_url)
+            param.push(options.qs[i].lat + ',' + options.qs[i].lng)            
         }
-        url += '&' + qs;
+        url += '&markers=' + param.join('|');
+    }
+    var getUrl = function () {
+        return url;
     }
 
-    var img = document.createElement('img');
-    img.setAttribute('src', url);
-    img.setAttribute('alt', options.alttext);
+    var createImage = function(container) {
+        if (!container) {
+            throw 'Cedarmaps: No container selector specified.';
+        } else if (!document.querySelector(container)) {
+            throw 'Cedarmaps: Could not find element ' + container;
+        }    
+    
+        var img = document.createElement('img');
+        img.setAttribute('src', url);
+        img.setAttribute('alt', options.alttext);
+    
+        var element = document.querySelector(container);
+        element.appendChild(img);
+    }    
 
-    var element = document.querySelector(options.container);
-    element.appendChild(img);
+    return {
+        getUrl: getUrl,
+        createImage: createImage
+    }
 }
