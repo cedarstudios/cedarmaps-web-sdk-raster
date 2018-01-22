397d2754eb5aed3e4e097d7f5b4b121991b64cf7
Kikojas Deployer <deployer@kikojas.com>
2018-01-22 14:40:46 +0330

    Removed legend control

--- src/map.js
+++ src/map.js
@@ -7,11 +7,13 @@ var MMap = require('mapbox.js/src/map'),
 function withAccessToken(options, accessToken) {
     if (!accessToken || options.accessToken)
         return options;
-    return L.extend({accessToken: accessToken}, options);
+    return L.extend({
+        accessToken: accessToken
+    }, options);
 }
 
 var CMap = MMap.Map.extend({
-    initialize: function(element, _, options) {
+    initialize: function (element, _, options) {
 
         MMap.Map.prototype.initialize.call(this, element, _,
             L.extend({}, MMap.Map.prototype.options, options));
@@ -26,15 +28,19 @@ var CMap = MMap.Map.extend({
         this.addControl(this._cedarmapsLogoControl);
     },
 
-    _initialize: function(json) {
+    _initialize: function (json) {
         MMap.Map.prototype._initialize.call(this, json);
 
-        this._cedarmapsLogoControl._setTileJSON(json);  
+        this._cedarmapsLogoControl._setTileJSON(json);
     }
 });
 
 module.exports.Map = CMap;
 
-module.exports.map = function(element, _, options) {
+module.exports.map = function (element, _, options) {
+    if (options) {
+        options.legendControl = false
+        options.attributionControl = false
+    }
     return new CMap(element, _, options);
 };
